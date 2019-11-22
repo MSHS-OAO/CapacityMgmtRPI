a62e2956420810eefbb4237fc974b97fffaf007f
Nevin <nevink01@mountsinai.org>
2019-11-22 16:37:29 -0500

    Function to determine week of year with Sat as first DOW

---
+++ TestWeekNumberFunction.R
@@ -0,0 +1,15 @@
+library(lubridate)
+
+a <- as.Date("8/4/19", format = "%m/%d/%y")
+
+weeknum <- function(x) {
+  yr <<- year(x)
+  new_yr <<- as.Date(paste0("1/1/", yr), format = "%m/%d/%Y")
+  new_yr_wkday <<- wday(new_yr, label = FALSE)
+  new_yr_sat <<- new_yr + (7 - new_yr_wkday)
+  elapsed_days <<- as.numeric(x - new_yr_sat)
+  week_number <<- ifelse(elapsed_days < 0, 1, as.integer(elapsed_days/7)+2)
+  week_number
+}
+
+weeknum(a)

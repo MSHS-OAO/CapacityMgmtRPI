9f6263e93c236ca448487d80bf08c17f5aa251e9
Nevin <nevink01@mountsinai.org>
2019-11-22 16:05:01 -0500

    More data formatting, lookups, and exclusions

--- WeeklyDischVolume.R
+++ WeeklyDischVolume.R
@@ -2,14 +2,60 @@
 library(readxl)
 library(ggplot2)
 library(lubridate)
+library(dplyr)
 
 getwd()
 setwd("J:\\Presidents\\HSPI-PM\\Operations Analytics and Optimization\\Projects\\Service Lines\\Capacity Management\\Data\\Discharge Billing Data")
 
 raw <- read.csv(choose.files(), header = TRUE, na.strings = c("", "NA"))
+
+ref_file <- "Analysis Reference 2019-11-22.xlsx"
+site_dict <- read_excel(ref_file, sheet = "Sites")
+dispo_dict <- read_excel(ref_file, sheet = "DischDispo")
+service_line_dict <- read_excel(ref_file, sheet = "ServiceLines")
+
+dispo_dict[is.na(dispo_dict$`Discharge Disposition Desc Msx`), 1] <- "Unknown"
+
+
 data <- raw
+# Format admit and discharge dates and pull out year, month, DOW, etc.
 data$AdmitDate <- as.Date(data$Admit.Dt.Src, "%m/%d/%Y")
+data$DischDate <- as.Date(data$Dsch.Dt.Src, "%m/%d/%Y")
 data$DischDateTime <- as.POSIXct(as.character(paste(data$Dsch.Dt.Src, data$Dsch.Time.Src)),  tz = "", format = "%m/%d/%Y %H:%M")
 data[ , c("AdmitYr", "AdmitMo")] <- c(year(data$AdmitDate), month(data$AdmitDate))
-data[ , c("DischYr", "DischMo", "DischHr", "DischDOW")] <- c(year(data$DischDateTime), month(data$DischDateTim), hour(data$DischDateTime), wday(data$DischDateTime, label = TRUE, abbr = TRUE))
+data[ , c("DischYr", "DischMo", "DischHr")] <- c(year(data$DischDate), month(data$DischDate), hour(data$DischDateTime))
+data$DischDOW <- wday(data$DischDate, label = TRUE, abbr = TRUE)
+
+# Site lookup
+data$Facility.Msx <- as.character(data$Facility.Msx)
+data <- left_join(data, site_dict, by = c("Facility.Msx" = "Facility Msx"))
+data$Site <- as.factor(data$Site)
+# data$Site <- site_dict[match(data$Facility.Msx, site_dict$`Facility Msx`), 2]
+# data <- merge(data, site_dict, by.x = "Facility.Msx", by.y = "Facility Msx", all.x = TRUE, all.y = FALSE, incomparables = NA)
+
+# Discharge disposition formatting and lookup
+data$Discharge.Disposition.Desc.Msx <- factor(data$Discharge.Disposition.Desc.Msx, levels = c(levels(data$Discharge.Disposition.Desc.Msx), "Unknown"))
+data[is.na(data$Discharge.Disposition.Desc.Msx), "Discharge.Disposition.Desc.Msx"] <- "Unknown"
+data$Discharge.Disposition.Desc.Msx <- as.character(data$Discharge.Disposition.Desc.Msx)
+data <- left_join(data, dispo_dict, by = c("Discharge.Disposition.Desc.Msx" = "Discharge Disposition Desc Msx"))
+colnames(data)[ncol(data)] <- "DispoRollUp"
+data$Discharge.Disposition.Desc.Msx <- as.factor(data$Discharge.Disposition.Desc.Msx)
+data$DispoRollUp <- as.factor(data$DispoRollUp)
+# data$Dispo <- dispo_dict[match(data$DischargeDispo, dispo_dict$`Discharge Disposition Desc Msx`), 2]
+
+# Service line inclusion / exclusion lookup
+colnames(data)[colnames(data) == "Service.Desc.Msx"] <- "ServiceLine"
+data$ServiceLine <- as.character(data$ServiceLine)
+data <- left_join(data, service_line_dict[ , c(1,3)], by = c("ServiceLine" = "Service Desc Msx"))
+colnames(data)[ncol(data)] <- "ServiceLineInclude"
+data$ServiceLine <- as.factor(data$ServiceLine)
+data$ServiceLineInclude <- as.factor(data$ServiceLineInclude)
+# data$ServiceLineInclude <- service_line_dict[match(data$ServiceLine, service_line_dict$`Service Desc Msx`), 3]
+
+data$Include <- ifelse(data$DispoRollUp == "Expired" | data$ServiceLineInclude == "No", FALSE, TRUE)
+
+data2 <- data[data$Include == TRUE, ]
+
+#Analysis for Jan-Sep 2019 benchmark
+data2$BaselineDate <- ifelse(data2$DischYr == 2019 & data2$DischMo <= 9, TRUE, FALSE)
 
